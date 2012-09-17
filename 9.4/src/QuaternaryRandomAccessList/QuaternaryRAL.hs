4f80997bbbdd96cc820837a0f31e3dc600f11826
Ken Kawamoto <kentaro@twitter.com>
2012-09-17 15:10:39 +0900

    merge lookup and update

--- 9.4/src/QuaternaryRandomAccessList/QuaternaryRAL.hs
+++ 9.4/src/QuaternaryRandomAccessList/QuaternaryRAL.hs
@@ -8,6 +8,8 @@ module QuaternaryRAL
        , Digit(..)
        ) where
 
+import Data.Monoid (mappend)
+
 import RandomAccessList
 
 {-
@@ -51,65 +53,67 @@ unconsTree (Digit2 t1 t2 : ds) = (t1, Digit1 t2 : ds)
 unconsTree (Digit3 t1 t2 t3 : ds) = (t1, Digit2 t2 t3 : ds)
 unconsTree (Digit4 t1 t2 t3 t4 : ds) = (t1, Digit3 t2 t3 t4 : ds)
 
-lookupDigit :: Ord a => Int -> [Digit a] -> Int -> a
-lookupDigit i [] _ = error "index out of bounds"
-lookupDigit i (Digit1 t1 : ds) r
-  | i < r * 1 = lookupTree (i - r * 0) t1 r
-  | otherwise = lookupDigit (i - r * 1) ds (r * 4)
-lookupDigit i (Digit2 t1 t2 : ds) r
-  | i < r * 1 = lookupTree (i - r * 0) t1 r
-  | i < r * 2 = lookupTree (i - r * 1) t2 r
-  | otherwise = lookupDigit (i - r * 2) ds (r * 4)
-lookupDigit i (Digit3 t1 t2 t3 : ds) r
-  | i < r * 1 = lookupTree (i - r * 0) t1 r
-  | i < r * 2 = lookupTree (i - r * 1) t2 r
-  | i < r * 3 = lookupTree (i - r * 2) t3 r
-  | otherwise = lookupDigit (i - r * 3) ds (r * 4)
-lookupDigit i (Digit4 t1 t2 t3 t4 : ds) r
-  | i < r * 1 = lookupTree (i - r * 0) t1 r
-  | i < r * 2 = lookupTree (i - r * 1) t2 r
-  | i < r * 3 = lookupTree (i - r * 2) t3 r
-  | i < r * 4 = lookupTree (i - r * 3) t4 r
-  | otherwise = lookupDigit (i - r * 4) ds (r * 4)
+data Context a = Context { frontDigits :: [Digit a]
+                         , rearDigits :: [Digit a]
+                         , digitContext :: DigitWalk a
+                         , treeContext :: [TreeWalk a]
+                         }
+data DigitWalk a = Digit1Go1
+                 | Digit2Go1 (Tree a)
+                 | Digit2Go2 (Tree a)
+                 | Digit3Go1 (Tree a) (Tree a)
+                 | Digit3Go2 (Tree a) (Tree a)
+                 | Digit3Go3 (Tree a) (Tree a)
+                 | Digit4Go1 (Tree a) (Tree a) (Tree a)
+                 | Digit4Go2 (Tree a) (Tree a) (Tree a)
+                 | Digit4Go3 (Tree a) (Tree a) (Tree a)
+                 | Digit4Go4 (Tree a) (Tree a) (Tree a)
+data TreeWalk a = GoDown1 (Tree a) (Tree a) (Tree a)
+                | GoDown2 (Tree a) (Tree a) (Tree a)
+                | GoDown3 (Tree a) (Tree a) (Tree a)
+                | GoDown4 (Tree a) (Tree a) (Tree a)
 
-lookupTree :: Ord a => Int -> Tree a -> Int -> a
-lookupTree 0 (Leaf v) _ = v
-lookupTree i (Node t1 t2 t3 t4) n
-  | i < m * 1 = lookupTree (i - m * 0) t1 m
-  | i < m * 2 = lookupTree (i - m * 1) t2 m
-  | i < m * 3 = lookupTree (i - m * 2) t3 m
-  | otherwise = lookupTree (i - m * 3) t4 m
-  where
-    m = n `div` 4
+initialCtx :: [Digit a] -> Context a
+initialCtx ds = Context { frontDigits = []
+                        , rearDigits = ds
+                        , digitContext = undefined
+                        , treeContext = []
+                        }
 
-updateDigit :: Ord a => Int -> a -> [Digit a] -> Int -> [Digit a]
-updateDigit i _ [] _ = error "index out of bounds"
-updateDigit i v (Digit1 t1 : ds) r
-  | i < r * 1 = Digit1 (updateTree (i - r * 0) v t1 r) : ds
-  | otherwise = Digit1 t1 : updateDigit (i - r * 1) v ds (r * 4)
-updateDigit i v (Digit2 t1 t2 : ds) r
-  | i < r * 1 = Digit2 (updateTree (i - r * 0) v t1 r) t2 : ds
-  | i < r * 2 = Digit2 t1 (updateTree (i - r * 1) v t2 r) : ds
-  | otherwise = Digit2 t1 t2 : updateDigit (i - r * 2) v ds (r * 4)
-updateDigit i v (Digit3 t1 t2 t3 : ds) r
-  | i < r * 1 = Digit3 (updateTree (i - r * 0) v t1 r) t2 t3 : ds
-  | i < r * 2 = Digit3 t1 (updateTree (i - r * 1) v t2 r) t3 : ds
-  | i < r * 3 = Digit3 t1 t2 (updateTree (i - r * 2) v t3 r) : ds
-  | otherwise = Digit3 t1 t2 t3 : updateDigit (i - r * 3) v ds (r * 4)
-updateDigit i v (Digit4 t1 t2 t3 t4 : ds) r
-  | i < r * 1 = Digit4 (updateTree (i - r * 0) v t1 r) t2 t3 t4 : ds
-  | i < r * 2 = Digit4 t1 (updateTree (i - r * 1) v t2 r) t3 t4 : ds
-  | i < r * 3 = Digit4 t1 t2 (updateTree (i - r * 2) v t3 r) t4 : ds
-  | i < r * 4 = Digit4 t1 t2 t3 (updateTree (i - r * 3) v t4 r) : ds
-  | otherwise = Digit4 t1 t2 t3 t4 : updateDigit (i - r * 4) v ds (r * 4)
+updateListCtx ctx d = Context { frontDigits = d : frontDigits ctx
+                              , rearDigits = Prelude.tail (rearDigits ctx)
+                              , digitContext = digitContext ctx
+                              , treeContext = treeContext ctx
+                              }
 
-updateTree :: Ord a => Int -> a -> Tree a -> Int -> Tree a
-updateTree 0 v (Leaf _) _ = (Leaf v)
-updateTree i v (Node t1 t2 t3 t4) n
-  | i < m * 1 = Node (updateTree (i - m * 0) v t1 m) t2 t3 t4
-  | i < m * 2 = Node t1 (updateTree (i - m * 1) v t2 m) t3 t4
-  | i < m * 3 = Node t1 t2 (updateTree (i - m * 2) v t3 m) t4
-  | otherwise = Node t1 t2 t3 (updateTree (i - m * 3) v t4 m)
+walkDigits :: Ord a => Int -> [Digit a] -> Int -> Context a -> (Tree a, Context a)
+walkDigits i [] _ _ = error "index out of bounds"
+walkDigits i (Digit1 t1 : ds) n ctx
+  | i < n * 1 = walkTree (i - n * 0) t1 n (ctx { digitContext = Digit1Go1 })
+  | otherwise = walkDigits (i - n * 1) ds (n * 4) (updateListCtx ctx (Digit1 t1))
+walkDigits i (Digit2 t1 t2 : ds) n ctx
+  | i < n * 1 = walkTree (i - n * 0) t1 n (ctx { digitContext = Digit2Go1 t2 })
+  | i < n * 2 = walkTree (i - n * 1) t2 n (ctx { digitContext = Digit2Go2 t1 })
+  | otherwise = walkDigits (i - n * 2) ds (n * 4) (updateListCtx ctx (Digit2 t1 t2))
+walkDigits i (Digit3 t1 t2 t3 : ds) n ctx
+  | i < n * 1 = walkTree (i - n * 0) t1 n (ctx { digitContext = Digit3Go1 t2 t3 })
+  | i < n * 2 = walkTree (i - n * 1) t2 n (ctx { digitContext = Digit3Go2 t1 t3 })
+  | i < n * 3 = walkTree (i - n * 2) t3 n (ctx { digitContext = Digit3Go3 t1 t2 })
+  | otherwise = walkDigits (i - n * 3) ds (n * 4) (updateListCtx ctx (Digit3 t1 t2 t3))
+walkDigits i (Digit4 t1 t2 t3 t4 : ds) n ctx
+  | i < n * 1 = walkTree (i - n * 0) t1 n (ctx { digitContext = Digit4Go1 t2 t3 t4 })
+  | i < n * 2 = walkTree (i - n * 1) t2 n (ctx { digitContext = Digit4Go2 t1 t3 t4 })
+  | i < n * 3 = walkTree (i - n * 2) t3 n (ctx { digitContext = Digit4Go3 t1 t2 t4 })
+  | i < n * 4 = walkTree (i - n * 3) t4 n (ctx { digitContext = Digit4Go4 t1 t2 t3 })
+  | otherwise = walkDigits (i - n * 4) ds (n * 4) (updateListCtx ctx (Digit4 t1 t2 t3 t4))
+
+walkTree :: Ord a => Int -> Tree a -> Int -> Context a -> (Tree a, Context a)
+walkTree 0 (Leaf v) _ ctx = (Leaf v, ctx)
+walkTree i (Node t1 t2 t3 t4) n ctx
+  | i < m * 1 = walkTree (i - m * 0) t1 m (ctx { treeContext = (GoDown1 t2 t3 t4) : treeContext ctx })
+  | i < m * 2 = walkTree (i - m * 1) t2 m (ctx { treeContext = (GoDown2 t1 t3 t4) : treeContext ctx })
+  | i < m * 3 = walkTree (i - m * 2) t3 m (ctx { treeContext = (GoDown3 t1 t2 t4) : treeContext ctx })
+  | otherwise = walkTree (i - m * 3) t4 m (ctx { treeContext = (GoDown4 t1 t2 t3) : treeContext ctx })
   where
     m = n `div` 4
 
@@ -127,5 +131,29 @@ instance RandomAccessList QuaternaryRAL where
 
   tail (QL ds) = QL $ (snd . unconsTree) ds
 
-  lookup i (QL ds) = lookupDigit i ds 1
-  update i v (QL ds) = QL $ updateDigit i v ds 1
+  lookup i (QL ds) = case walkDigits i ds 1 (initialCtx ds) of
+    (Leaf v, _) -> v
+    _           -> error "walkDigits shouldn't return non-leaf node"
+
+  update i v (QL ds) = QL $ mappend (reverse frontDigits) (newDigit digitCtx : rearDigits)
+    where
+      (Leaf _, Context frontDigits (_ : rearDigits) digitCtx treeCtx) = walkDigits i ds 1 (initialCtx ds)
+
+      newDigit Digit1Go1 = Digit1 newTree
+      newDigit (Digit2Go1 t2) = Digit2 newTree t2
+      newDigit (Digit2Go2 t1) = Digit2 t1 newTree
+      newDigit (Digit3Go1 t2 t3) = Digit3 newTree t2 t3
+      newDigit (Digit3Go2 t1 t3) = Digit3 t1 newTree t3
+      newDigit (Digit3Go3 t1 t2) = Digit3 t1 t2 newTree
+      newDigit (Digit4Go1 t2 t3 t4) = Digit4 newTree t2 t3 t4
+      newDigit (Digit4Go2 t1 t3 t4) = Digit4 t1 newTree t3 t4
+      newDigit (Digit4Go3 t1 t2 t4) = Digit4 t1 t2 newTree t4
+      newDigit (Digit4Go4 t1 t2 t3) = Digit4 t1 t2 t3 newTree
+
+      newTree = constructTree (Leaf v) treeCtx
+
+      constructTree t [] = t
+      constructTree t (GoDown1 t2 t3 t4 : ws) = constructTree (Node t t2 t3 t4) ws
+      constructTree t (GoDown2 t1 t3 t4 : ws) = constructTree (Node t1 t t3 t4) ws
+      constructTree t (GoDown3 t1 t2 t4 : ws) = constructTree (Node t1 t2 t t4) ws
+      constructTree t (GoDown4 t1 t2 t3 : ws) = constructTree (Node t1 t2 t3 t) ws
